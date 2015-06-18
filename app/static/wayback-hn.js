3bdc80fef7dfc0d3154103149fb5fb72a172fb45
Jonathan Dubin <mail@jondubin.com>
2015-06-18 00:49:39 -0400

    For the time being moved js into index.html

--- app/static/wayback-hn.js
+++ app/static/wayback-hn.js
@@ -1,3 +1,16 @@
 /**
  * Created by dubin on 5/29/15.
  */
+
+
+//var current_mode =
+//
+//
+//$(function() {
+//    $("input:radio[name=mode]").click(function () {
+//        var value = $(this).val();
+//        $("#random-text").text(value);
+//
+//        $("#random-link").attr("href", "/?date=random-" + value);
+//    });
+//});
